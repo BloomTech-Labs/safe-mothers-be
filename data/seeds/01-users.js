ae09fb8dd9ba74461d25684bfb9d4da6511b7f1f
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-10-15 16:27:29 -0600

    Renamed files

---
+++ data/seeds/01-users.js
@@ -0,0 +1,15 @@
+
+const bcrypt = require("bcryptjs");
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('users').insert([
+    {
+      username:"admin",
+      password: bcrypt.hashSync("password"),
+      first_name: "admin",
+      last_name: "admin"
+    },
+  ])
+    
+};
