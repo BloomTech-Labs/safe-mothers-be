3d631d8f45668a4750f1ded4496d5c3b40debe38
Annique Nsabimana <anniquensabimana@gmail.com>
2019-10-16 17:34:46 -0500

    Ran users Test

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -1,56 +1,17 @@
 const router = require("express").Router();
 
-const Users = require("./usersHelper");
+const users = require("./usersHelper");
 
-router.get("/", (req, res) => {
-  Users.getUsers()
-    .catch(err => {
-      res.status(200).json({ message: "Error getting users" });
-    })
-    .then(users => {
-      res.status(200).json(users);
-    });
-});
+const restricted = require("../auth/restrictedMiddleware.js")
+router.get('/', restricted, (req, res) => {
+  return users.find()
+       .then(user => {
+           res.status(200).json({loggedInUser: req.user.username, user})
+       })
+       .catch(err => {
+           res.status(400).json({ message: 'Could not retrieve users, make sure you are logged in.' })
+       })
+})
 
-router.get("/:id", (req, res) => {
-  let { id } = req.params;
-
-  Users.findBy({ id })
-    .catch(error => {
-      res.status(500).json(error);
-    })
-    .then(users => {
-      if (users.length === 0) {
-        return res
-          .status(404)
-          .json({ message: `No user with the ID of ${id}` });
-      }
-
-      const user = users[0];
-      res.status(200).json({
-        name: user.name,
-        username: user.username
-      });
-    });
-});
-
-router.delete("/:id", (req, res) => {
-  const { id } = req.params;
-  Users.deleteUser(id)
-    .catch(err => {
-      res.status(500).json({ message: `Error deleting user: ${err}` });
-    })
-    .then(numChanges => {
-      if (numChanges === 0) {
-        return res
-          .status(404)
-          .json({ message: `Could not delete user with ID ${id}` });
-      } else {
-        return res
-          .status(200)
-          .json({ message: `User with ID ${id} has been deleted` });
-      }
-    });
-});
 
 module.exports = router;
