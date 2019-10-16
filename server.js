ed66899ccd907de770788169e89b76b0b5cad67a
denmercs <denmercs@gmail.com>
2019-10-15 23:40:45 -0500

    done with mothers endpoint, working on drivers

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
-const express = require('express')
-const cors = require('cors')
-const helmet = require('helmet')
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 
+const authRouter = require("./auth/authRouter");
+const restricted = require("./auth/restrictedMiddleware");
+const usersRouter = require("./users/usersRouter");
+const mothersRouter = require("./mothers/mothersRouter");
 
-const authRouter = require('./auth/authRouter')
-const restricted = require('./auth/restrictedMiddleware')
-const usersRouter = require('./users/usersRouter')
+const server = express();
 
-const server = express()
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
 
-server.use(helmet())
-server.use(cors())
-server.use(express.json())
+server.use("/auth", authRouter);
+server.use("/users", restricted, usersRouter);
+server.use("/mothers", restricted, mothersRouter);
 
-server.use('/auth', authRouter)
-server.use('/users', restricted, usersRouter)
+server.get("/", (req, res) => {
+  res.json({ api: "safe mothers" });
+});
 
-server.get('/', (req, res) => {
-  res.json({ api: 'safe mothers' })
-})
-
-module.exports = server
+module.exports = server;
--- users/usersHelper.js
+++ users/usersHelper.js
@@ -1,28 +1,28 @@
-const db = require('../data/dbConfig')
+const db = require("../data/dbConfig");
 
 module.exports = {
   getUsers,
   addUser,
   findBy,
   deleteUser
+};
+
+function getUsers() {
+  return db("users").select("username", "first_name", "last_name");
 }
 
-function getUsers () {
-  return db('users').select('users.id', 'users.name', 'users.username')
+async function addUser(user) {
+  let ids = await db("users").insert(user, "id");
+  const [id] = ids;
+  return findBy({ id });
 }
 
-async function addUser (user) {
-  let ids = await db('users').insert(user, 'id')
-  const [id] = ids
-  return findBy({ id })
+function findBy(filter) {
+  return db("users").where(filter);
 }
 
-function findBy (filter) {
-  return db('users').where(filter)
-}
-
-function deleteUser (id) {
-  return db('users')
+function deleteUser(id) {
+  return db("users")
     .del()
-    .where({ id })
+    .where({ id });
 }
