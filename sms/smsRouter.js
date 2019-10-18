9f0cfd58718d916dcee17db1730bad99a70551e0
denmercs <denmercs@gmail.com>
2019-10-18 04:57:34 -0500

    testing SMS functionality through registering the mother

--- sms/smsRouter.js
+++ sms/smsRouter.js
@@ -27,17 +27,19 @@ router.get("/drivers", (req, res) => {
 });
 
 // get driver by id
-router.get('/drivers/:id', async (req, res) => {
-    const { id } = req.params;
-    Drivers.getDriverById(id)
-      .then(drivers => {
-          if (drivers) {
-            res.status(200).json(drivers);
-          } else {
-              res.status(404).json({ message: 'Could not find driver with associated id.'});
-          }
-      })
-      .catch(err => res.status(500).json(error));
+router.get("/drivers/:id", async (req, res) => {
+  const { id } = req.params;
+  Drivers.getDriverById(id)
+    .then(drivers => {
+      if (drivers) {
+        res.status(200).json(drivers);
+      } else {
+        res
+          .status(404)
+          .json({ message: "Could not find driver with associated id." });
+      }
+    })
+    .catch(err => res.status(500).json(error));
 });
 
 // edit a driver based on ID
@@ -65,63 +67,68 @@ router.delete("/drivers/:id", (req, res) => {
 
 //Unprotected Mother Routes, SMS Functionaity
 
-// register mother
-router.post("/mothers/register", (req, res) => {
-    let data = req.body;
-  
-    Mothers.addMother(data)
-      .then(mother => {
-        res.status(201).json({ message: "Added a mother" });
-      })
-      .catch(err => {
-        res.status(500).json(err);
-      });
-  });
-  
-  // get all mothers
-  router.get("/mothers", (req, res) => {
-    Mothers.getMothers()
-      .then(mothers => {
-        res.status(200).json(mothers);
-      })
-      .catch(err => res.status(500).json(err));
-  });
+// register mother through SMS
+router.post("/mothers/register/:phone_number/:village_id", (req, res) => {
+  let phone_number = req.params.phone_number;
+  let village_id = parseInt(req.params.village_id);
 
-  // get mothers by id
-router.get('/mothers/:id', async (req, res) => {
-    const { id } = req.params;
-    Mothers.getMotherById(id)
-      .then(mothers => {
-          if (mothers) {
-            res.status(200).json(mothers);
-          } else {
-              res.status(404).json({ message: 'Could not find mother with associated id.'});
-          }
-      })
-      .catch(err => res.status(500).json(error));
+  let data = { phone_number: phone_number, village_id: village_id };
+
+  Mothers.addMother(data)
+    .then(mother => {
+      res.status(201).json({ message: "Added a mother" });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
-  
-  // edit a mother based on ID
-  router.put("/mothers/:id", (req, res) => {
-    const { id } = req.params;
-    const data = req.body;
-  
-    Mothers.updateMother(id, data)
-      .then(mothers => {
+
+// get all mothers
+router.get("/mothers", (req, res) => {
+  Mothers.getMothers()
+    .then(mothers => {
+      res.status(200).json(mothers);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+// get mothers by id
+router.get("/mothers/:id", async (req, res) => {
+  const { id } = req.params;
+  Mothers.getMotherById(id)
+    .then(mothers => {
+      if (mothers) {
         res.status(200).json(mothers);
-      })
-      .catch(err => res.status(500).json(err));
-  });
-  
-  // delete a mother based on ID
-  router.delete("/mothers/:id", (req, res) => {
-    const { id } = req.params;
-  
-    Mothers.deleteMother(id)
-      .then(mothers => {
-        res.status(200).json({ message: "mother deleted!" });
-      })
-      .catch(err => res.status(500).json(err));
-  });
+      } else {
+        res
+          .status(404)
+          .json({ message: "Could not find mother with associated id." });
+      }
+    })
+    .catch(err => res.status(500).json(error));
+});
+
+// edit a mother based on ID
+router.put("/mothers/:id", (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+
+  Mothers.updateMother(id, data)
+    .then(mothers => {
+      res.status(200).json(mothers);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+// delete a mother based on ID
+router.delete("/mothers/:id", (req, res) => {
+  const { id } = req.params;
+
+  Mothers.deleteMother(id)
+    .then(mothers => {
+      res.status(200).json({ message: "mother deleted!" });
+    })
+    .catch(err => res.status(500).json(err));
+});
 
 module.exports = router;
