5a8b52d9d2b649e529b1d34f906044dd9c389e97
denmercs <denmercs@gmail.com>
2019-10-29 14:28:36 -0500

    fixing frontline help end points - driver phone number

--- sms/smsRouter.js
+++ sms/smsRouter.js
@@ -71,13 +71,14 @@ router.get("/mothers/help/:phone_number", async (req, res) => {
         .then(request => {
           /** This is just temporary, we will do the 5 minutes response time filter */
           let message = `${drivers[0].name} have a request pending pickup id of ${request}. To confirm type "answer pickupID" (example: yes 12)`;
-          sendDataToFrontlineSMS(message, newNum);
-          console.log(phone_number);
+          sendDataToFrontlineSMS(message, drivers[0].phone_number);
+          // console.log(phone_number);
+          console.log(drivers[0].phone_number);
         })
         .catch(err => console.log(err));
     } else {
       let message = `To register type "register village" (example: register Iganga)`;
-      sendDataToFrontlineSMS(message, newNum);
+      sendDataToFrontlineSMS(message, drivers[0].phone_number);
     }
   } catch (err) {
     console.log(err);
