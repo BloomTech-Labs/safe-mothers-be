58c68854b7c9ee62e87feee19c3002fe059bcf6f
Jonathan Dorety <kyullog@gmail.com>
2019-11-12 08:57:14 -0800

    Configure middleware for testing

--- auth/restrictedMiddleware.js
+++ auth/restrictedMiddleware.js
@@ -1,21 +1,25 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const secrets = require('../config/secrets.js');
+const secrets = require("../config/secrets.js");
+
+const testToken = process.env.TEST_TOKEN || null;
 
 module.exports = (req, res, next) => {
   // const token = req.header('x-auth-token')
   const token = req.headers.authorization;
-
-  if (token) {
+  if (testToken) {
+    req.user = { username: "testerosa" };
+    next();
+  } else if (token) {
     jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
       if (err) {
-        res.status(401).json({ you: 'The credentials provided are wrong' });
+        res.status(401).json({ you: "The credentials provided are wrong" });
       } else {
         req.user = { username: decodedToken.username };
         next();
       }
     });
   } else {
-    res.status(400).json({ message: 'No Credential Provider' });
+    res.status(400).json({ message: "No Credential Provider" });
   }
 };
