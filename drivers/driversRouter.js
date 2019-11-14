db975719a2e5fff378b36a7f294f5498af75e410
Jonathan Dorety <kyullog@gmail.com>
2019-11-13 16:01:01 -0800

    Fix / Polish delete, register endpoints

--- drivers/driversRouter.js
+++ drivers/driversRouter.js
@@ -1,23 +1,28 @@
-const router = require('express').Router();
-const Drivers = require('./driversHelper');
+const router = require("express").Router();
+const Drivers = require("./driversHelper");
 
 // register driver
 
-router.post('/register', (req, res) => {
-
+router.post("/register", (req, res) => {
   let data = req.body;
-
-  Drivers.addDriver(data)
-    .then(driver => {
-      res.status(201).json({ message: 'Added a driver' });
-    })
-    .catch(err => {
-      res.status(500).json(err);
-    });
+  if (!data.driver_name || !data.phone) {
+    res.status(400).json({ message: "Driver name and phone # required" });
+  } else if (!data.latitude || !data.longitude) {
+    res.status(400).json({ message: "Driver latitude & longitude required" });
+  } else {
+    Drivers.addDriver(data)
+      .then(driver => {
+        res.status(201).json({ message: "Added a driver" });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 
 // get all the drivers
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   Drivers.getDrivers()
     .then(drivers => {
       res.status(200).json(drivers);
@@ -26,7 +31,7 @@ router.get('/', (req, res) => {
 });
 
 // get driver by id
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
   Drivers.getDriverById(id)
     .then(drivers => {
@@ -35,7 +40,7 @@ router.get('/:id', async (req, res) => {
       } else {
         res
           .status(404)
-          .json({ message: 'Could not find driver with associated id.' });
+          .json({ message: "Could not find driver with associated id." });
       }
     })
     .catch(err => res.status(500).json(err));
@@ -44,18 +49,18 @@ router.get('/:id', async (req, res) => {
 //Get drivers who are available for the dashboard
 router.get("/available/drivers", (req, res) => {
   Drivers.getDriverAvailability()
-  .then(available => {
-    console.log(available)
-    res.status(200).json(available)
-  })
-  .catch(err => {
-    console.log(err)
-    res.status(500).json(err)
-  })
-})
+    .then(available => {
+      console.log(available);
+      res.status(200).json(available);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 // edit a driver based on ID
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   const { id } = req.params;
   const data = req.body;
 
@@ -67,12 +72,16 @@ router.put('/:id', (req, res) => {
 });
 
 // delete a driver based on ID
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   const { id } = req.params;
 
   Drivers.deleteDriver(id)
-    .then(drivers => {
-      res.status(200).json({ message: 'Driver deleted!' });
+    .then(driver => {
+      if (driver) {
+        res.status(200).json({ message: `${id} deleted` });
+      } else {
+        res.status(404).json({ message: `No driver associated with id ${id}` });
+      }
     })
     .catch(err => res.status(500).json(err));
 });
