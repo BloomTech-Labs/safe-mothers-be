0b43f6524262d21f61ebfe62437f859518d85bf0
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-11-21 14:03:31 -0700

    Changed driver seeds to match Safe Mothers actual driver list from old Django backend.

--- data/seeds/03-drivers.js
+++ data/seeds/03-drivers.js
@@ -1,102 +1,80 @@
 exports.seed = function(knex) {
   return knex("drivers").insert([
-    //please correct the phone numbers of the actual drivers
-    {
-      driver_name: "Jaden Lopez",
-      latitude: "0.8683849999999976",
-      longitude: "33.382454999999936",
-      phone: "+19093794021",
-      availability: true,
-      reliability: 4,
-      online:false
-    },
-    {
-      driver_name: "Johnathan Dorety",
-      latitude: "0.8607573",
-      longitude: "33.4388595",
-      phone: "+13366959254",
-      availability: true,
-      reliability: 4,
-      online:true
-    },
-
-    {
-      driver_name: "Deer Belieff",
-      latitude: "1.8254328",
-      longitude: "50.4547664",
-      phone: "+699699695",
-      availability: true,
-      reliability: 3,
-      online:true
-    },
-    //Make Mugeere's phone a team members number
+    
+    //These are Safe Mothers actual drivers. Please do not erase!!!!!
+    //***WARNING***: For Lambda Labs Teams testing the SMS system, please change and save the below phone numbers. These are actual working phone numbers in Uganda.
     {
       driver_name: "Nviiri Brian",
       latitude: "0.7954328",
       longitude: "33.4547664",
-      phone: "+16055217234",
+      phone: "+256782407942",
       availability: true,
       reliability: 4,
       online:false
     },
     {
-      driver_name: "Mugeere Erifazi",
-      latitude: "0.8254328",
-      longitude: "34.524547664",
-      phone: "+16084670303",
-      availability: true,
-      reliability: 5,
-      online:true
-    },
-    {
-      driver_name: "Zijjakyiro Bakali",
-      latitude: "0.8407573",
-      longitude: "32.4388595",
-      phone: "+256750684130699",
-      availability: true,
-      reliability: 5,
-      online:true
-    },
-
-    {
-      driver_name: "Dembe Erifazi",
-      latitude: "-0.847919",
-      longitude: "30.905961",
-      phone: "+699699679699",
-      availability: false,
-      reliability: 5,
-      online:true
-    },
-
-    {
       driver_name: "Kalende Yasin",
-      latitude: "0.8307573",
-      longitude: "32.4388595",
-      phone: "+256789563155699",
+      latitude: "0.8607573",
+      longitude: "33.4388595",
+      phone: "+256789563155",
       availability: true,
       reliability: 5,
-      online:true
+      online:false
     },
-    
     {
       driver_name: "Izimba Jowari",
-      latitude: "-1.847919",
-      longitude: "30.905961",
-      phone: "+25678327095699",
+      latitude: "0.8607573",
+      longitude: "33.4388595",
+      phone: "+25678327095",
       availability: true,
       reliability: 5,
-      online:true
+      online:false
     },
     {
       driver_name: "Muduuli Aminsi",
-      latitude: "0.9607573",
-      longitude: "35.4388595",
-      phone: "+25678965912699",
+      latitude: "0.8607573",
+      longitude: "33.4388595",
+      phone: "+25678965912",
       availability: true,
       reliability: 5,
-      online:true
+      online:false
     },
-
+    {
+      driver_name: "Zijjakyiro Bakali",
+      latitude: "0.8607573",
+      longitude: "33.4388595",
+      phone: "+256783269299",
+      availability: true,
+      reliability: 5,
+      online:false
+    },
+    {
+      driver_name: "Zijjakyiro Bakali",
+      latitude: "0.8607573",
+      longitude: "33.4388595",
+      phone: "+256750684130",
+      availability: true,
+      reliability: 5,
+      online:false
+    },
+    {
+      driver_name: "Mugeere Erifazi",
+      latitude: "0.8607573",
+      longitude: "33.4388595",
+      phone: "+256777634545",
+      availability: true,
+      reliability: 5,
+      online:false
+    },
+    {
+      driver_name: "Dembe Erifazi",
+      latitude: "0.7954328",
+      longitude: "33.4547664",
+      phone: "+699699679699",
+      availability: true,
+      reliability: 5,
+      online:false
+    },
 
   ]);
 };
